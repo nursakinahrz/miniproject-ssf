09d5d8c0ab6520a596c56bb191a990a33a956879
Nursakinah <nursakinah.arz@gmail.com>
2022-08-18 22:16:00 +0800

    inital commit, added system.properties + dependencies

---
+++ src/main/java/ssf/miniproject/map/MapApplication.java
@@ -0,0 +1,13 @@
+package ssf.miniproject.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/miniproject/gmap/GmapApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.miniproject.gmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
